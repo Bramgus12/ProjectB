ec9e0f09d603455426ff98a1b934be6200151dcc
Bram Gussekloo <0966476@hr.nl>
2019-03-03 16:48:42 +0100

    login screen ready. credentials are: username: Test Password: Test
    Crashes when entering credentials

--- app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
+++ app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
@@ -1,14 +1,16 @@
 package com.bramgussekloo.projectb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//http://hackpundit.com/simple-login-android-app/ Bron
+    //http://hackpundit.com/simple-login-android-app/ Bron
     private EditText username;
     private EditText password;
     private Button login_button;
@@ -20,17 +22,21 @@ public class MainActivity extends AppCompatActivity {
         LoginButton();
     }
 
-    public void LoginButton(){
-        username = (EditText)findViewById(R.id.Username);
-        password = (EditText)findViewById(R.id.Password);
-        login_button = (Button)findViewById(R.id.LoginButton);
+    public void LoginButton() {
+        username = (EditText) findViewById(R.id.Username);
+        password = (EditText) findViewById(R.id.Password);
+        login_button = (Button) findViewById(R.id.LoginButton);
 
         login_button.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (username.getText().toString().equals("Test") && password.getText().toString().equals("Test")){
-
+                        if (username.getText().toString().equals("Test") && password.getText().toString().equals("Test")) {
+                            Toast.makeText(MainActivity.this, "Username and password is correct.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent("com.bramgussekloo.projectb.User");
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Username or password is not correct. Try again", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
