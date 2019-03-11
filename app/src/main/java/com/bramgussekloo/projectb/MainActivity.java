49822cb9c89145c146654aac5f99b15b8b9fa20a
Bram Gussekloo <0966476@hr.nl>
2019-03-11 15:35:44 +0100

    added register page

--- app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
+++ app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
@@ -1,5 +1,4 @@
 package com.bramgussekloo.projectb;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,18 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
     //http://hackpundit.com/simple-login-android-app/ Bron
     private EditText username;
     private EditText password;
     private Button login_button;
+    private Button register_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LoginButton();
+        RegisterButton();
     }
 
     public void LoginButton() {
@@ -31,20 +31,17 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         if (username.getText().toString().equals("Test") && password.getText().toString().equals("Test")) {
                             Toast.makeText(MainActivity.this, "Username and password are correct.", Toast.LENGTH_SHORT).show();
-                            Intent Userintent = new Intent(getBaseContext(),User.class);
+                            Intent Userintent = new Intent(getBaseContext(), User.class);
                             startActivity(Userintent);
-                        }
-                        else if(username.getText().toString().equals("TestAdmin") && password.getText().toString().equals("TestAdmin")){
-                            Toast.makeText(MainActivity.this,"Username and password are correct.", Toast.LENGTH_SHORT).show();
-                            Intent Adminintent = new Intent(getBaseContext(),Admin.class);
-                            startActivity(Adminintent);
-                        }
-                        else if(username.getText().toString().equals("TestBeheerder") && password.getText().toString().equals("TestBeheerder")){
+                        } else if (username.getText().toString().equals("TestAdmin") && password.getText().toString().equals("TestAdmin")) {
                             Toast.makeText(MainActivity.this, "Username and password are correct.", Toast.LENGTH_SHORT).show();
-                            Intent BeheerderIntent = new Intent(getBaseContext(),Beheerder.class);
+                            Intent Adminintent = new Intent(getBaseContext(), Admin.class);
+                            startActivity(Adminintent);
+                        } else if (username.getText().toString().equals("TestBeheerder") && password.getText().toString().equals("TestBeheerder")) {
+                            Toast.makeText(MainActivity.this, "Username and password are correct.", Toast.LENGTH_SHORT).show();
+                            Intent BeheerderIntent = new Intent(getBaseContext(), Beheerder.class);
                             startActivity(BeheerderIntent);
-                        }
-                        else {
+                        } else {
                             Toast.makeText(MainActivity.this, "Username or password is not correct. Try again", Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -52,4 +49,18 @@ public class MainActivity extends AppCompatActivity {
         );
 
     }
+    public void RegisterButton() {
+        register_button = findViewById(R.id.RegisterButton);
+        register_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent Registerintent = new Intent(getBaseContext(), Register.class);
+                        startActivity(Registerintent);
+                    }
+
+                }
+        );
+    }
 }
+
