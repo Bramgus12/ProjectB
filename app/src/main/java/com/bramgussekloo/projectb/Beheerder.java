fb3919977e61c44d7a3d87a4d7623ad674c9c281
MohamedLammaoui <mlammaoui@gmail.com>
2019-03-14 22:11:35 +0100

    RecyclerView + RecyclerViewAdapter + Product Model + Product Activity Bijbehorende Layouts. Menu eruit gehaald, zodat elke Activity zijn eigen menu kan hebben, moet later worden gei

--- app/src/main/java/com/bramgussekloo/projectb/Beheerder.java
+++ app/src/main/java/com/bramgussekloo/projectb/Beheerder.java
@@ -1,13 +1,28 @@
 package com.bramgussekloo.projectb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Beheerder extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beheerder);
+
+        Button productlistActivity = (Button) findViewById(R.id.productList);
+        productlistActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), ProductActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
     }
 }
