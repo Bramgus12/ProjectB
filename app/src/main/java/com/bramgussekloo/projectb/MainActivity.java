3152e7779703ce0209445a129848e239dd502db4
Bram Gussekloo <bram.gussekloo@gmail.com>
2019-03-22 14:33:31 +0100

    Login and register works

--- app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
+++ app/src/main/java/com/bramgussekloo/projectb/MainActivity.java
@@ -1,12 +1,20 @@
 package com.bramgussekloo.projectb;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText email;
@@ -16,12 +24,14 @@ public class MainActivity extends AppCompatActivity {
     private TextInputLayout emailLayout;
     private TextInputLayout passwordLayout;
     private Button recyclerview_button;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LoginButton();
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         login_button = findViewById(R.id.LoginButton);
         register_button = findViewById(R.id.RegisterButton);
         recyclerview_button = findViewById(R.id.recyclerViewButton);
+
         register_button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -52,6 +63,36 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email == null || email.getText().toString().trim().isEmpty()){
+                    emailLayout.setError("Email required.");
+                    email.requestFocus();
+                    return;
+                }
+                if (password == null || password.getText().toString().trim().isEmpty()){
+                    passwordLayout.setError("Password required.");
+                    password.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString().trim()).matches()){
+                    email.setError("Enter a valid email.");
+                    email.requestFocus();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Log in failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
