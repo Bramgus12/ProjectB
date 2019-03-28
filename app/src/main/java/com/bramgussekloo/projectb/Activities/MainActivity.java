09024da0aa49058e6c46a892f2870e651373e26e
MohamedLammaoui <mlammaoui@gmail.com>
2019-03-28 15:53:13 +0100

    - Gebruikers hoeven niet meer opnieuw in te loggen als ze al reeds aangemeld zijn. - Tijdelijke menubar toegevoegd, waarbij je kan uitloggen. - Progressbar toegevoegd bij registreren & inloggen.

--- app/src/main/java/com/bramgussekloo/projectb/Activities/MainActivity.java
+++ app/src/main/java/com/bramgussekloo/projectb/Activities/MainActivity.java
@@ -2,12 +2,14 @@ package com.bramgussekloo.projectb.Activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bramgussekloo.projectb.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -42,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser != null){
             getUser();
         }
-
 
     }
 
